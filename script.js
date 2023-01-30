6f9c9f1c2f32d4ab4375668b17d4867c646168c6
Upasana <112039921+Igdtuwresource@users.noreply.github.com>
2023-01-30 15:40:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,131 @@
+let products = {
+    data: [
+      {
+        productName: "Engineering Mechanics",
+        category: "Buy-EM-book",
+        
+        image: "em.jpeg",
+        url: "",
+      },
+      {
+        productName: "Programming with Python",
+        category: "Buy-Python-book",
+        
+        image: "python.jpeg",
+        url: "",
+      },
+
+      {
+        productName: "Communication Skills",
+        category: "Buy-CS-book",
+        
+        image: "cs.jpeg",
+        url: "",
+      },
+      
+      
+      
+    ],
+  };
+  
+  for (let i of products.data) {
+    //Create Card
+    let card = document.createElement("div");
+    //Card should have category and should stay hidden initially
+    card.classList.add("card", i.category, "hide");
+    //image div
+    let imgContainer = document.createElement("div");
+    imgContainer.classList.add("image-container");
+    //img tag
+    let image = document.createElement("img");
+    image.setAttribute("src", i.image);
+    imgContainer.appendChild(image);
+    card.appendChild(imgContainer);
+    //container
+    let container = document.createElement("div");
+    container.classList.add("container");
+    //product name
+    let name = document.createElement("h5");
+    name.classList.add("product-name");
+    name.innerText = i.productName.toUpperCase();
+    container.appendChild(name);
+   
+    
+    var a = document.createElement('a'); 
+                  
+                // Create the text node for anchor element.
+                var link = document.createTextNode(i.category);
+                  
+                // Append the text node to anchor element.
+                a.appendChild(link); 
+                  
+                // Set the title.
+                a.title = i.category; 
+                  
+                // Set the href property.
+                a.href = i.url; 
+                  
+                // Append the anchor element to the body.
+                container.appendChild(a);
+    
+    card.appendChild(container);
+    document.getElementById("products").appendChild(card);
+  }
+  
+  //parameter passed from button (Parameter same as category)
+  function filterProduct(value) {
+    //Button class code
+    let buttons = document.querySelectorAll(".button-value");
+    buttons.forEach((button) => {
+      //check if value equals innerText
+      if (value.toUpperCase() == button.innerText.toUpperCase()) {
+        button.classList.add("active");
+      } else {
+        button.classList.remove("active");
+      }
+    });
+  
+    //select all cards
+    let elements = document.querySelectorAll(".card");
+    //loop through all cards
+    elements.forEach((element) => {
+      //display all cards on 'all' button click
+      if (value == "all") {
+        element.classList.remove("hide");
+      } else {
+        //Check if element contains category class
+        if (element.classList.contains(value)) {
+          //display element based on category
+          element.classList.remove("hide");
+        } else {
+          //hide other elements
+          element.classList.add("hide");
+        }
+      }
+    });
+  }
+  
+  //Search button click
+  document.getElementById("search").addEventListener("click", () => {
+    //initializations
+    let searchInput = document.getElementById("search-input").value;
+    let elements = document.querySelectorAll(".product-name");
+    let cards = document.querySelectorAll(".card");
+  
+    //loop through all elements
+    elements.forEach((element, index) => {
+      //check if text includes the search value
+      if (element.innerText.includes(searchInput.toUpperCase())) {
+        //display matching card
+        cards[index].classList.remove("hide");
+      } else {
+        //hide others
+        cards[index].classList.add("hide");
+      }
+    });
+  });
+  
+  //Initially display all products
+  window.onload = () => {
+    filterProduct("all");
+  };
